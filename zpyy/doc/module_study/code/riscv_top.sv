bca9d9bc290078b127a4808437f069b8fd0e8743
wen8tang <wenbatang@126.com>
2026-03-14 21:51:01 +0800

    删除仿真文件

--- zpyy/doc/module_study/code/riscv_top.sv
+++ zpyy/doc/module_study/code/riscv_top.sv
@@ -8,25 +8,32 @@ module riscv_top #(
     input logic                     rst
 );
 
-logic                       jump_en_i;
-logic [`ADD_WIDTH-1:0]      pc_o;
-logic [`ADD_WIDTH-1:0]      jump_addr_i;
-logic [`DATA_WIDTH-1:0]     inst_data_o;
-logic [`ADD_WIDTH-1:0]      inst_add_o;
-logic [`DATA_WIDTH-1:0]     inst_o;
+logic                           jump_en_i;
+logic [`ADD_WIDTH-1:0]          pc_o;
+logic [`ADD_WIDTH-1:0]          jump_addr_i;
+logic [`DATA_WIDTH-1:0]         inst_data_o;
+logic [`ADD_WIDTH-1:0]          inst_add_o;
+logic [`DATA_WIDTH-1:0]         inst_o;
 
-logic [`REG_ADDR_WIDTH-1:0]   rs1_addr_o;
-logic [`REG_ADDR_WIDTH-1:0]   rs2_addr_o;
-logic [`REG_ADDR_WIDTH-1:0]   rd_addr_o;
-logic [`DATA_WIDTH-1:0]       rs1_data_i;
-logic [`DATA_WIDTH-1:0]       rs2_data_i;
-logic [`DATA_WIDTH-1:0]       rs1_data_o;
-logic [`DATA_WIDTH-1:0]       rs2_data_o;
+logic [`REG_ADDR_WIDTH-1:0]     rs1_addr_o;
+logic [`REG_ADDR_WIDTH-1:0]     rs2_addr_o;
+logic [`REG_ADDR_WIDTH-1:0]     rd_addr_o;
+logic [`DATA_WIDTH-1:0]         rs1_data_i;
+logic [`DATA_WIDTH-1:0]         rs2_data_i;
+logic [`DATA_WIDTH-1:0]         rs1_data_o;
+logic [`DATA_WIDTH-1:0]         rs2_data_o;
+logic                           wr_reg_en_i;
+logic [`REG_ADDR_WIDTH-1:0]     wr_reg_addr_i;
+logic [`DATA_WIDTH-1:0]         wr_reg_data_i;
 
-assign jump_en_i        = 1'b0;                // 暂时不使用跳转功能
-assign jump_addr_i      = {`ADD_WIDTH{1'b0}}; // 跳转地址
-assign rs1_data_i      = `DATA_WIDTH'd50;  // 寄存器数据输入，暂时不连接寄存器堆
-assign rs2_data_i      = `DATA_WIDTH'd100; //
+
+assign jump_en_i        = 1'b0;                     // 暂时不使用跳转功能
+assign jump_addr_i      = {`ADD_WIDTH{1'b0}};       // 跳转地址
+assign rs1_data_i       = `DATA_WIDTH'd50;          // 寄存器数据输入，暂时不连接寄存器堆
+assign rs2_data_i       = `DATA_WIDTH'd100;         
+assign wr_reg_en_i      = 1'b1;                     // 暂时不使用写回功能
+assign wr_reg_addr_i    = rd_addr_o;                // 写回寄存器地址
+assign wr_reg_data_i    = rs1_data_o + rs2_data_o;  // 写回数据，暂时设置为rs1和rs2数据的和
 
 pc u_pc (
     .clk         	(clk         ),
